a10e3c99cd30fb484ac33e099783333d2abccd98
Alessandro Cascioli <alexcascioli@hotmail.it>
2023-08-24 18:19:39 +0200

    hidden trainingtracker

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
         <PageLoader />
       ) : (
         <div className="px-4 md:px-8 prefers-color-scheme-tremor space-y-6">
-          <TrainingTracker />
+          {/* <TrainingTracker /> */}
           <KPIs />
         </div>
       )}
